c19e8910451bd07ca94150cfd8e35e4f53d10510
Sérgio Horácio <54984958+shfnenas@users.noreply.github.com>
2022-09-16 14:24:22 -0300

    Update Desafio2.sql

--- Desafio2.sql
+++ Desafio2.sql
@@ -1,14 +1,14 @@
 SELECT	ContasAPagar.Numero AS NumeroProcessoPagamento, Pessoa.Nome AS NomeFornecedor, 
 		ContasAPagar.DataVencimento, ContasPagas.DataPagamento, ContasAPagar.Valor AS ValorLiquido, 
-                         CASE WHEN ContasPagas.DataPagamento IS NULL THEN 'À Pagar' ELSE 'Paga' END AS Identificador
+                         CASE WHEN ContasPagas.DataPagamento IS NULL THEN 'Ã€ Pagar' ELSE 'Paga' END AS Identificador
 FROM            ContasAPagar LEFT OUTER JOIN
                          ContasPagas ON ContasAPagar.Numero = ContasPagas.Numero LEFT OUTER JOIN
                          Pessoa ON ContasAPagar.CodigoFornecedor = Pessoa.Codigo
 
 /*
-O Gerente financeiro requisitou junto a Globaltec, 
-uma consulta que traga as informações de contas a pagar e contas pagas, 
+O Gerente financeiro requisitou junto a xxx, 
+uma consulta que traga as informaÃ§Ãµes de contas a pagar e contas pagas, 
 sendo que ele precisa 
-do número do processo de pagamento, nome do fornecedor, data de vencimento, data de pagamento, valor líquido 
-e um identificador se é conta a pagar ou paga.
+do nÃºmero do processo de pagamento, nome do fornecedor, data de vencimento, data de pagamento, valor lÃ­quido 
+e um identificador se Ã© conta a pagar ou paga.
 */
